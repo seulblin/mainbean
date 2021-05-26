0efce14ee8bbd9d7513a5d8753e3f4b9ef968325
seulblin <subinge94@gmail.com>
2021-05-27 01:25:59 +0900

    html script제이쿼리 주소 변경

--- dist/newstyle.fafaa89b.js
+++ dist/newstyle.fafaa89b.js
@@ -117,24 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"js.js":[function(require,module,exports) {
-$(document).ready(function () {
-  //click 이벤트
-  $('#arroWrap').click(function () {
-    $('#arrow1').toggleClass('rotater');
-    $('#arrow2').toggleClass('rotatel');
-    $('.customer').toggleClass('customerOpen');
-  }); //Tabmenu 이벤트
+})({"C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-  $('.resume-content > li').click(function () {
-    var index = $(this).index();
-    $('.resume-content').removeClass('on');
-    $('.resume-content').eq(index).addClass('on');
-    $('.tabMenu').hide();
-    $('.tabMenu').eq(index).show();
-  });
-});
-},{}],"C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"newstyle.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./img\\1.jpg":[["1.dc197a9a.jpg","img/1.jpg"],"img/1.jpg"],"./img\\2.jpg":[["2.3ca6bb44.jpg","img/2.jpg"],"img/2.jpg"],"./img\\3.jpg":[["3.a433b89d.jpg","img/3.jpg"],"img/3.jpg"],"./img\\4.jpg":[["4.c6d96be5.jpg","img/4.jpg"],"img/4.jpg"],"./img\\background.png":[["background.59c06501.png","img/background.png"],"img/background.png"],"./img\\skill.png":[["skill.58b7c286.png","img/skill.png"],"img/skill.png"],"./img\\s.jpg":[["s.fa914ab1.jpg","img/s.jpg"],"img/s.jpg"],"./img\\wokr1.jpg":[["wokr1.d2c62cab.jpg","img/wokr1.jpg"],"img/wokr1.jpg"],"./img\\wokr2.jpg":[["wokr2.5b4971c4.jpg","img/wokr2.jpg"],"img/wokr2.jpg"],"./img\\wokr3.jpg":[["wokr3.1ac01227.jpg","img/wokr3.jpg"],"img/wokr3.jpg"],"./img\\wokr4.jpg":[["wokr4.00c2514b.jpg","img/wokr4.jpg"],"img/wokr4.jpg"],"./img\\wokr5.jpg":[["wokr5.ca1fd28a.jpg","img/wokr5.jpg"],"img/wokr5.jpg"],"./img\\wokr6.jpg":[["wokr6.74d99c00.jpg","img/wokr6.jpg"],"img/wokr6.jpg"],"./img\\keyword.png":[["keyword.66521dd6.png","img/keyword.png"],"img/keyword.png"],"_css_loader":"C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -338,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js.js"], null)
-//# sourceMappingURL=/js.00934245.js.map
+},{}]},{},["C:/Users/이슬빈/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/newstyle.fafaa89b.js.map
